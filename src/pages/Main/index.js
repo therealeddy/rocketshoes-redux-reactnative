b5f2b7b65bd769e4d3ad34552114c555a036211b
therealeddy <eduardo.mfonseca@hotmail.com>
2020-01-18 01:35:54 -0300

    Main Front Complete

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { Container } from './styles';
+import { ScrollView, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
-export default function Main() {
-  return (
-    <Container />
-  );
+import {
+  Container,
+  Product,
+  ListProducts,
+  Preview,
+  Title,
+  Content,
+  Price,
+  ButtonCart,
+  Count,
+  ButtonCartText,
+  AmountText,
+  AreaTextButton
+} from './styles';
+
+export default class Main extends Component {
+  state = {
+    product: []
+  }
+
+  async componentDidMount() {
+    const response = await api.get('/products');
+    this.setState({ product: response.data });
+  }
+
+  render() {
+    const { product } = this.state;
+
+    return (
+      <Container>
+        <ScrollView horizontal>
+          <ListProducts>
+            {product.map(item => (
+              <Product key={String(item.id)}>
+                <Preview source={{ uri: item.image }} />
+                <Content>
+                  <View>
+                    <Title>{ item.title }</Title>
+                    <Price>{ item.price }</Price>
+                  </View>
+                  <ButtonCart>
+                    <Count>
+                      <Icon name="add-shopping-cart" color="#FFF" size={15} />
+                      <AmountText>0</AmountText>
+                    </Count>
+                    <AreaTextButton>
+                      <ButtonCartText>
+                        Adicionar
+                      </ButtonCartText>
+                    </AreaTextButton>
+                  </ButtonCart>
+                </Content>
+              </Product>
+            ))}
+          </ListProducts>
+        </ScrollView>
+      </Container>
+    );
+  }
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,7 +1,80 @@
 import styled from 'styled-components/native';
 
+import { RectButton } from 'react-native-gesture-handler';
+
 export const Container = styled.View`
   flex: 1;
-  padding: 30px;
+  padding: 30px 0;
   background-color: #191920;
+`;
+
+export const Product = styled.View`
+  width: 220px;
+  height: 380px;
+  background: #fff;
+  border-radius: 4px;
+  margin: 15px;
+  padding: 10px;
+`;
+
+export const ListProducts = styled.View`
+  flex-direction: row;
+`;
+
+export const Preview = styled.Image`
+  width: 200px;
+  height: 200px;
+  margin-bottom: 10px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  justify-content: space-between;
+`;
+
+export const Title = styled.Text`
+  font-size: 16px;
+  margin-bottom: 5px;
+`;
+
+export const Price = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const ButtonCart = styled(RectButton)`
+  width: 100%;
+  height: 40px;
+  border-radius: 5px;
+  background: #7159C1;
+  flex-direction: row;
+
+`;
+
+export const Count = styled.View`
+  background-color: rgba(0,0,0,0.2);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 25%;
+  padding: 0 12px;
+`;
+
+export const AmountText = styled.Text`
+  color: #fff;
+  font-size: 12px;
+`;
+
+export const AreaTextButton = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonCartText = styled.Text`
+  text-transform: uppercase;
+  color: #fff;
+  font-weight: bold;
 `;
